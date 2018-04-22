3fab7756bbd9070be599fc8c5c5dd40d12d28a82
Bence Ludmány <mokkants@Bences-MacBook-Air.local>
2018-04-22 12:18:35 +0200

    Tjena, värld

---
+++ greeting/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting implements a library of a newborn baby's first actions
+package greeting
+
+// HelloWorld says hello to the brave new world of Go
+func HelloWorld() string {
+	return "Hello, World!"
+}
